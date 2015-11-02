1f6e0b2f495c2e3d001ac524f415f616eb186196
Pawit Pornkitprasan <p.pawit@gmail.com>
2015-11-02 00:02:36 -0500

    base: Option to use volume keys to control ring volume anytime (1/2)
    AICP Note: this used to be called
    Option to use volume keys to control media volume anytime
    
    With changes in P making the media volume the default stream, it makes
    more sense to add ring tone volume as the option instead.
    
    Squashed with:
    Fix forcing media stream control on tablet
    There's option volume_keys_control_ring_stream (Volume keys control ringtone
    volume / If on, volume keys control ringtone volume. If off, volume keys
    control media volume.) in Settings which is also available on tablets but it
    doesn't work on these devices (I didn't find reported issue).
    As I like this option and think it may be even more wanted on media devices
    such as tablets, here's a fix.
    
    PureNexus edits:
    *modified to force media stream when toggled on, ring/notification stream when off
    
    ABC edits: use existing content resolver variable
    use one void for the setting getter
    enabled by default
    
    AICP edits:
     * disabled by default
     * brought up to P by @eyosen
    
    Change-Id: I8c0c02a9205c1accdb5404183b7fda9a0eab749b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4549,6 +4549,17 @@ public final class Settings {
         public static final String BLUETOOTH_SHOW_BATTERY = "bluetooth_show_battery";
 
         /**
+         * Volume key controls ringtone or media sound stream
+         * @hide
+         */
+        public static final String VOLUME_KEYS_CONTROL_RING_TONE =
+                "volume_keys_control_ring_tone";
+
+        /** @hide */
+        private static final Validator VOLUME_KEYS_CONTROL_RING_TONE_VALIDATOR =
+               BOOLEAN_VALIDATOR;
+
+        /**
          * Whether to display the 4g or lte network icon in the statusbar
          * @hide
          */
@@ -4668,6 +4679,7 @@ public final class Settings {
             NOTIFICATION_VIBRATION_INTENSITY,
             HAPTIC_FEEDBACK_INTENSITY,
             DISPLAY_COLOR_MODE,
+
             STATUS_BAR_CLOCK,
             STATUSBAR_CLOCK_STYLE,
             STATUS_BAR_CLOCK_SECONDS,
@@ -4677,6 +4689,7 @@ public final class Settings {
             STATUSBAR_CLOCK_DATE_FORMAT,
             USE_BOTTOM_GESTURE_NAVIGATION,
             ONE_HAND_MODE_ENABLED,
+            VOLUME_KEYS_CONTROL_RING_TONE,
         };
 
         /**
@@ -4810,6 +4823,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUS_BAR_QUICK_QS_PULLDOWN);
             PRIVATE_SETTINGS.add(ONE_HAND_MODE_ENABLED);
             PRIVATE_SETTINGS.add(STATUS_BAR_LOGO);
+            PRIVATE_SETTINGS.add(VOLUME_KEYS_CONTROL_RING_TONE);
         }
 
         /**
@@ -4908,6 +4922,7 @@ public final class Settings {
             VALIDATORS.put(USE_BOTTOM_GESTURE_NAVIGATION,
                     USE_BOTTOM_GESTURE_NAVIGATION_VALIDATOR);
             VALIDATORS.put(ONE_HAND_MODE_ENABLED, ONE_HAND_MODE_ENABLED_VALIDATOR);
+            VALIDATORS.put(VOLUME_KEYS_CONTROL_RING_TONE,VOLUME_KEYS_CONTROL_RING_TONE_VALIDATOR);
         }
 
         /**
