6447e2a1e4cbd0854cbab04fe3d6351d6cb9f4d9
maxwen <max.weninger@gmail.com>
2016-01-03 17:11:54 +0100

    QS auto brightness toggle rework
    - use different vector images and improve layout
    
    - fix color
    
    Change-Id: Iaa678ee81e20c3d212642297c8ff49bb40a72232

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
@@ -173,8 +173,8 @@ public class BrightnessMirrorController
         mIcon = (ImageView) mBrightnessMirror.findViewById(R.id.brightness_icon);
         if (mIcon != null) {
             mIcon.setImageResource(automatic ?
-                    com.android.systemui.R.drawable.ic_qs_brightness_auto_on :
-                    com.android.systemui.R.drawable.ic_qs_brightness_auto_off);
+                    com.android.systemui.R.drawable.ic_qs_brightness_auto_on_new :
+                    com.android.systemui.R.drawable.ic_qs_brightness_auto_off_new);
         }
         mIconLeft = (ImageView) mBrightnessMirror.findViewById(R.id.brightness_icon_left);
         if (mIconLeft != null) {
