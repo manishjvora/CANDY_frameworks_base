e9230066597da197b04e8574fd61f649d37d4635
ezio84 <brabus84@gmail.com>
2018-02-11 01:56:44 +0100

    Fix notifications for some apps if Less notification sounds is enabled
    e.g. latest whatsapp
    
    notifications from same pkg can have the same currentTime
    the notification key is constructed like
    
    String sbnKey = user.getIdentifier() + "|" + pkg + "|" + id + "|" + tag + "|" + uid;
    
    So those notification will be separated using key
    
    Change-Id: I0cb8a50297db1d4edbe2eb0bb84dd39a594748f6

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -379,6 +379,11 @@ public class NotificationManagerService extends SystemService {
     // The last key in this list owns the hardware.
     ArrayList<String> mLights = new ArrayList<>();
 
+    @GuardedBy("mNotificationLock")
+    private HashMap<String, Long> mAnnoyingNotifications = new HashMap<String, Long>();
+
+    private long mAnnoyingNotificationThreshold = 30000; // 30 seconds
+
     private AppOpsManager mAppOps;
     private UsageStatsManagerInternal mAppUsageStats;
     private DevicePolicyManagerInternal mDpm;
@@ -4561,6 +4566,27 @@ public class NotificationManagerService extends SystemService {
                     }
                 }
             }
+        }
+    }
+
+    @GuardedBy("mNotificationLock")
+    private boolean notificationIsAnnoying(String key, String pkg) {
+        if (key == null
+                || mAnnoyingNotificationThreshold <= 0
+                || (pkg != null && "android".equals(pkg))) {
+            return false;
+        }
+
+        long currentTime = System.currentTimeMillis();
+        if (mAnnoyingNotifications.containsKey(key)
+                && (currentTime - mAnnoyingNotifications.get(key)
+                < mAnnoyingNotificationThreshold)) {
+            // less than threshold; it's an annoying notification!!
+            return true;
+        } else {
+            // not in map or time to re-add
+            mAnnoyingNotifications.put(key, currentTime);
+            return false;
         }
     }
 
