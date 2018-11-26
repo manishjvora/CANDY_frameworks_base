437f8195ad8c9b06da7e42c9bb71434fce00f5b3
Pranav Vashi <neobuddy89@gmail.com>
2018-11-26 15:56:50 +0530

    Fixes for lockscreen visualizer
    * In some cases LS visualizer is no-go. This patch addresses same and optimizes code.
    
    Change-Id: Icac7ff2a35e4acf789119b0f6039b89291d19a4f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/VisualizerView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/VisualizerView.java
@@ -173,7 +173,7 @@ public class VisualizerView extends View
 
     private void updateViewVisibility() {
         final int curVis = getVisibility();
-        final int newVis = mStatusBarState != StatusBarState.SHADE
+        final int newVis = mVisible && mStatusBarState != StatusBarState.SHADE
                 && mVisualizerEnabled ? View.VISIBLE : View.GONE;
         if (curVis != newVis) {
             setVisibility(newVis);
@@ -184,6 +184,10 @@ public class VisualizerView extends View
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
+
+        if (mCurrentBitmap == null)
+            setColor(Color.TRANSPARENT);
+
         mSettingObserver = new SettingsObserver(new Handler());
         mSettingObserver.observe();
         mSettingObserver.update();
@@ -233,13 +237,11 @@ public class VisualizerView extends View
     }
 
     public void setVisible(boolean visible) {
-        if (mVisible != visible) {
-            if (DEBUG) {
-                Log.i(TAG, "setVisible() called with visible = [" + visible + "]");
-            }
-            mVisible = visible;
-            checkStateChanged();
+        if (DEBUG) {
+            Log.i(TAG, "setVisible() called with visible = [" + visible + "]");
         }
+        mVisible = visible;
+        updateViewVisibility();
     }
 
     public void setDozing(boolean dozing) {
@@ -348,7 +350,8 @@ public class VisualizerView extends View
     }
 
     private void checkStateChanged() {
-        if (getVisibility() == View.VISIBLE && mVisible && mPlaying && !mDozing && !mPowerSaveMode
+        boolean isVisible = getVisibility() == View.VISIBLE;
+        if (isVisible && mPlaying && !mDozing && !mPowerSaveMode
                 && mVisualizerEnabled && !mOccluded) {
             if (!mDisplaying) {
                 mDisplaying = true;
@@ -362,7 +365,7 @@ public class VisualizerView extends View
             if (mDisplaying) {
                 unlink();
                 mDisplaying = false;
-                if (mVisible) {
+                if (isVisible) {
                     animate()
                             .alpha(0f)
                             .setDuration(600);
