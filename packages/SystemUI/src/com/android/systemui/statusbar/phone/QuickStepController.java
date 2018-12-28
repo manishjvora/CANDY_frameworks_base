mike2587/abc_frameworks_base

Allow to set a full gesture navbar (swipe left for back action) [1/2]
swipe left for back button action
keeps showing the back button when pinning or when showing pin unlock

This doesn't break left swipe action to scroll apps when Overview is showing
after swiping right on navbar

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStepController.java
@@ -43,6 +43,7 @@ import android.util.Log;
 import android.util.Slog;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.KeyEvent;
 import android.view.WindowManagerGlobal;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
@@ -55,6 +56,7 @@ import com.android.systemui.shared.recents.IOverviewProxy;
 import com.android.systemui.shared.recents.utilities.Utilities;
 import com.android.systemui.shared.system.NavigationBarCompat;
 import com.android.internal.graphics.ColorUtils;
+import com.android.internal.util.du.Utils;
 
 /**
  * Class to detect gestures on the navigation bar and implement quick scrub.
@@ -84,6 +86,8 @@ public class QuickStepController implements GestureHelper {
     private AnimatorSet mTrackAnimator;
     private ButtonDispatcher mHitTarget;
     private View mCurrentNavigationBarView;
+
+    private boolean mBackActionScheduled;
 
     private final Handler mHandler = new Handler();
     private final Rect mTrackRect = new Rect();
@@ -267,6 +271,17 @@ public class QuickStepController implements GestureHelper {
                     offset -= mIsVertical ? mTrackRect.height() : mTrackRect.width();
                 }
 
+                final boolean allowBackAction = !mNavigationBarView.isFullGestureMode() ? false
+                        : (!mDragPositive ? offset < 0 && pos > touchDown
+                        : offset >= 0 && pos < touchDown);
+                // if quickscrub is active, don't trigger the back action but allow quickscrub drag
+                // action so the user can still switch apps
+                if (!mQuickScrubActive && exceededScrubTouchSlop && allowBackAction) {
+                    // schedule a back button action and skip quickscrub
+                     mBackActionScheduled = true;
+                    break;
+                }
+
                 final boolean allowDrag = !mDragPositive
                         ? offset < 0 && pos < touchDown : offset >= 0 && pos > touchDown;
                 float scrubFraction = Utilities.clamp(Math.abs(offset) * 1f / trackSize, 0, 1);
@@ -291,8 +306,15 @@ public class QuickStepController implements GestureHelper {
                 break;
             }
             case MotionEvent.ACTION_CANCEL:
+                mBackActionScheduled = false;
+                break;
             case MotionEvent.ACTION_UP:
-                endQuickScrub(true /* animate */);
+                if (mBackActionScheduled) {
+                    mBackActionScheduled = false;
+                    Utils.sendKeycode(KeyEvent.KEYCODE_BACK);
+                } else {
+                    endQuickScrub(true /* animate */);
+                }
                 break;
         }
 

manishjvora/CANDY_frameworks_base

Change-Id: I7529d9330be0843e31877933a3d8f311c93ad551

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStepController.java
@@ -46,6 +46,7 @@ import android.util.Log;
 import android.util.Slog;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.KeyEvent;
 import android.view.WindowManagerGlobal;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
@@ -58,6 +59,7 @@ import com.android.systemui.shared.recents.IOverviewProxy;
 import com.android.systemui.shared.recents.utilities.Utilities;
 import com.android.systemui.shared.system.NavigationBarCompat;
 import com.android.internal.graphics.ColorUtils;
+import com.android.internal.util.candy.CandyUtils;
 
 /**
  * Class to detect gestures on the navigation bar and implement quick scrub.
@@ -88,6 +90,8 @@ public class QuickStepController implements GestureHelper {
     private AnimatorSet mTrackAnimator;
     private ButtonDispatcher mHitTarget;
     private View mCurrentNavigationBarView;
+
+    private boolean mBackActionScheduled;
 
     private final Handler mHandler = new Handler();
     private final Rect mTrackRect = new Rect();
@@ -273,6 +277,17 @@ public class QuickStepController implements GestureHelper {
                     offset -= mIsVertical ? mTrackRect.height() : mTrackRect.width();
                 }
 
+                final boolean allowBackAction = !mNavigationBarView.isFullGestureMode() ? false
+                        : (!mDragPositive ? offset < 0 && pos > touchDown
+                        : offset >= 0 && pos < touchDown);
+                // if quickscrub is active, don't trigger the back action but allow quickscrub drag
+                // action so the user can still switch apps
+                if (!mQuickScrubActive && exceededScrubTouchSlop && allowBackAction) {
+                    // schedule a back button action and skip quickscrub
+                     mBackActionScheduled = true;
+                    break;
+                }
+
                 final boolean allowDrag = !mDragPositive
                         ? offset < 0 && pos < touchDown : offset >= 0 && pos > touchDown;
                 float scrubFraction = Utilities.clamp(Math.abs(offset) * 1f / trackSize, 0, 1);
@@ -299,8 +314,15 @@ public class QuickStepController implements GestureHelper {
                 break;
             }
             case MotionEvent.ACTION_CANCEL:
+                mBackActionScheduled = false;
+                break;
             case MotionEvent.ACTION_UP:
-                endQuickScrub(true /* animate */);
+                if (mBackActionScheduled) {
+                    mBackActionScheduled = false;
+                    CandyUtils.sendKeycode(KeyEvent.KEYCODE_BACK);
+                } else {
+                    endQuickScrub(true /* animate */);
+                }
                 break;
         }
 